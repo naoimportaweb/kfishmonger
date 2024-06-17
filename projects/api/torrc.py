aecb4a028e2fc9e1782039ec4db36ba8a2bf22c6
naoimportaweb <nao.importa.web@gmail.com>
2024-06-17 01:05:59 -0300

    Excluindo nós perigosos

--- projects/api/torrc.py
+++ projects/api/torrc.py
@@ -14,10 +14,18 @@ class Torrc(Config):
         if self.findattribute("HTTPTunnelPort") == None:
             self.addattribute("HTTPTunnelPort " + str(port));
 
+    def exclude14eyes(self):
+        if self.findattribute("ExcludeNodes") == None:
+            self.addattribute("ExcludeNodes {us},{uk},{gb},{ca},{il},{nl},{no},{dk},{au},{nz},{fr},{de},{be},{se},{es},{it}");     
+        if self.findattribute("StrictNodes") == None:
+            self.addattribute("StrictNodes 1");
 def main():
     t = Torrc();
     t.runasdaemon();
     t.tunnelport(9051);
+    t.exclude14eyes();
     t.save();
 if __name__ == "__main__":
     main();
+
+
