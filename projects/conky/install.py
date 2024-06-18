e60c4efba5e024fda2c522bb5fe447cc60cd70f1
naoimportaweb <nao.importa.web@gmail.com>
2024-06-17 23:39:23 -0300

    Alterando permissao de acesso ao diretorio do conky, o debian e chato

--- projects/conky/install.py
+++ projects/conky/install.py
@@ -17,9 +17,8 @@ def main():
     # RUN INSTALL SCRIPT
     process = Process("python3 " + path_script);
     process.run();
-    process = Process("chown -R " + os.getlogin() + ":" + os.getlogin() + " /opt/kfishmonger/projects/conky");
+    process = Process("chown -R " + os.getlogin() + ":" + os.getlogin() + " /opt/kfishmonger");
     process.run();
-
 
 if __name__ == "__main__":
     main();
