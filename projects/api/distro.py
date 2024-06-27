c5627851995f86b64b5e255ea2ada5cc2591cb36
naoimportaweb <nao.importa.web@gmail.com>
2024-06-27 11:07:24 -0300

    Username nos linux é diferente

--- projects/api/distro.py
+++ projects/api/distro.py
@@ -11,6 +11,11 @@ class Distro():
             print("Nao foi possivel detectar.");
     
     def user(self):
+        try:
+            buffer =  os.getlogin();
+            return buffer;
+        except:
+            print("Nao tem getlogin neste linux, vamos procurar pelas variáveis de ambiente.");
         tags = ["SUDO_USER", "USERNAME", "USERNAME", "USER"];
         for tag in tags:
             if os.environ.get( tag ) != "" and os.environ.get( tag ) != "root":
