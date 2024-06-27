2b39228a9c4fc890fbdca87b296d32f9ce6992c7
nao.importa.web <nao.importa.web@gmail.com>
2024-06-27 08:53:56 -0300

    customizando script para mint

--- projects/api/pip.py
+++ projects/api/pip.py
@@ -4,9 +4,14 @@ from .distro import Distro;
 class Pip():
     def install(self, package):
         distro = Distro();
-        p = Process("pip3 install " + package + " --root-user-action=ignore");
-        #if distro.name == "debian":
-        #    p = Process("pip3 install " + package + " --break-system-packages");
+        print("DISTRO NAME", distro.name());
+        
+        if distro.name() == "debian":
+            p = Process("pip3 install " + package + " --break-system-packages");
+        elif distro.name() == "ubuntu":
+            p = Process("pip3 install " + package );
+        else:
+            p = Process("pip3 install " + package + " --root-user-action=ignore");
         p.run();
 
 
