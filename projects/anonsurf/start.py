a65e5ee09f73a4139fc9c9a73e6a76afad8e2389
nao.importa.web <nao.importa.web@gmail.com>
2024-07-12 19:35:28 -0300

    Corrigindo erros

--- projects/anonsurf/start.py
+++ projects/anonsurf/start.py
@@ -3,6 +3,16 @@ import sys, os, shutil, time, hashlib, netifaces, json, traceback
 sys.path.insert(0,"/opt/kfishmonger/projects/");
 
 from api.process import Process;
+from api.systemctl import Systemctl
 
-p = Process("anonsurf", wait=False);
-print( p.run() );
+from api.log import Log;
+log = Log("anonsurf");
+
+s = Systemctl("vpn.service");
+if s.running():
+    p = Process("anonsurf", wait=False);
+    log.info("Iniciando Anonsurf");
+    print( p.run() );
+else:
+    log.info("O script Anonsurf não pode ser executado pois a VPN está ligada.");
+
