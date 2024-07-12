1ac0fb9ac298055fe6c3025ba7e31baf5ae4cf3c
nao.importa.web <nao.importa.web@gmail.com>
2024-07-12 19:08:24 -0300

    Corrigindo erros

--- projects/api/config_project.py
+++ projects/api/config_project.py
@@ -4,6 +4,8 @@ class ConfigProject():
     def __init__(self, project, log=None):
         self.log = log;
         self.project = project;
+        self.PATH_VAR = "/var/kfm/" + self.project + "/config.json";
+        self.PATH_OPT = "/opt/kfishmonger/projects/" + self.project + "/resources/config.json";
         if not os.path.exists("/var/kfm/" + self.project):
             os.makedirs("/var/kfm/" + self.project);
             if self.log != None:
@@ -12,20 +14,20 @@ class ConfigProject():
     def copy(self):
         js_opt = {};
         js_var = {};
-        if os.path.exists( "/opt/kfishmonger/projects/" + self.project + "/resources/config.json" ):
-            js_opt = json.loads(open("/opt/kfishmonger/projects/" + self.project + "/resources/config.json","r").read());
-        if os.path.exists("/var/kfm/" + self.project + "/resources/config.json"):
-            js_var = json.loads(open("/var/kfm/" + self.project + "/config.json","r").read());
+        if os.path.exists( self.PATH_OPT ):
+            js_opt = json.loads(open( self.PATH_OPT ,"r").read());
+        if os.path.exists(self.PATH_VAR):
+            js_var = json.loads(open(self.PATH_VAR,"r").read());
         for key in js_opt:
             if js_var.get( key ) == None:
                 js_var[key] = js_opt[key];
-        with open("/var/kfm/" + self.project + "/config.json","w") as f:
+        with open(self.PATH_VAR,"w") as f:
             f.write( json.dumps( js_var ) );
             if self.log != None:
-                self.log.info("Configuração salva em: /var/kfm/" + self.project + "/config.json");
+                self.log.info("Configuração salva em: " + self.PATH_VAR);
 
     def load(self):
-        js_var = json.loads(open("/var/kfm/" + self.project + "/config.json","r").read());
+        js_var = json.loads(open(self.PATH_VAR,"r").read());
         for key in js_var:
             setattr(self, key, js_var[key]);
         
