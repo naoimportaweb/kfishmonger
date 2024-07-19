106021c3aa4b1642759e3d3756e9dc4bd51165e3
nao.importa.web <nao.importa.web@gmail.com>
2024-07-18 22:43:39 -0300

    Fiz várias melhorias no código

--- projects/json/sub/tor.py
+++ projects/json/sub/tor.py
@@ -14,5 +14,8 @@ socket.socket = socks.socksocket
 r = request.urlopen('https://wtfismyip.com/json')
 js_myip = json.loads(r.read());
 
+# Tem que voltar o socket sem o proxy
+socket.socket = _socket
+
 jsonclient = JsonClient();
 jsonclient.send( {"tor" : {"country" : js_myip["YourFuckingCountry"], "ip" : js_myip["YourFuckingIPAddress"] }} );
