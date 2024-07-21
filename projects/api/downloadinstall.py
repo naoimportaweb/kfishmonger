c25e099e07c36575194feee549a59b858ed66ee6
nao.importa.web <nao.importa.web@gmail.com>
2024-07-20 22:38:10 -0300

    Fiz o instalador do MoneroD

--- projects/api/downloadinstall.py
+++ projects/api/downloadinstall.py
@@ -9,7 +9,7 @@ sys.path.append(CURRENTDIR);
 from api.process import Process;
 
 class DownloadInstall():
-    def __init__(self, file, url):
+    def __init__(self, file, url, log=None):
         self.url = url;
         self.file = file;
         self.tmp = "/tmp/" + str(uuid.uuid4());
@@ -30,27 +30,32 @@ class DownloadInstall():
             else:
                 os.chmod(path, st.st_mode | stat.S_IRGRP | stat.S_IROTH);
 
-    def download(self):
-        if os.path.exists("/tmp/" + self.file):
+    def download(self, force=True):
+        if os.path.exists("/tmp/" + self.file) and force == True:
             os.unlink("/tmp/" + self.file);
-        process = Process("wget "+ self.url +" -O /tmp/" + self.file);
-        process.run();
+        if not os.path.exists("/tmp/" + self.file):
+            process = Process("wget "+ self.url +" -O /tmp/" + self.file);
+            process.run();
         if self.file.find("tar.gz") > 0:
             process = Process("tar xzvf /tmp/" + self.file + " -C "+ self.tmp +" --strip-components 1");
             process.run();
         elif self.file.find(".zip") > 0:
             process = Process("unzip /tmp/" + self.file + " -d "+ self.tmp +"");
             process.run();
+        elif self.file.find("tar.bz2") > 0:
+            process = Process("tar -xjvf /tmp/"+ self.file +" -C "+ self.tmp  +" --strip-components 1");
+            process.run();
         else:
-            print(self.file);
             process = Process("tar --strip-components 1 -C "+ self.tmp +" -xf /tmp/" + self.file);
             process.run();
+        return self.tmp;
 
-    def extract(self, path):
+    def extract(self, path, permission=True):
         if not os.path.exists(path):
             os.makedirs(path, exist_ok=True);
         shutil.copytree(self.tmp, path , dirs_exist_ok=True)
-        self.__permission__(path);
+        if permission == True:
+            self.__permission__(path);
     
     def make(self):
         self.download();
---
+++ projects/tools/sub/monerod/install.py
@@ -0,0 +1,34 @@
+import sys, os, shutil, inspect;
+
+CURRENTDIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())));
+ROOT = os.path.dirname(os.path.dirname(os.path.dirname(CURRENTDIR)));
+sys.path.append(ROOT);
+
+from api.apt import Apt;
+from api.downloadinstall import DownloadInstall;
+from api.log import Log;
+from api.systemctl import Systemctl;
+
+def main():
+    if not os.path.exists("/opt/monero"):
+        os.makedirs("/opt/monero");
+    if not os.path.exists("/opt/monero/block"):
+        os.makedirs("/opt/monero/block");
+    if not os.path.exists("/opt/monero/bin"):
+        os.makedirs("/opt/monero/bin");
+    log = Log("monero");
+    d = DownloadInstall("monerod.tar.bz2", "https://downloads.getmonero.org/linux64");
+    d.download(force=False);
+    d.extract("/opt/monero/bin", permission=False);
+    shutil.copy( CURRENTDIR + "/resources/monerod.service", "/etc/systemd/system/");
+    ctl = Systemctl( "monerod.service" , log=log);
+    ctl.reload();
+    ctl.start();
+    ctl.enable();
+    if ctl.status():
+        print("Rodando o serviço MoneroD");
+    else:
+        print("O serviço MoneroD não está ligado.");
+
+if __name__ == "__main__":
+    main();
