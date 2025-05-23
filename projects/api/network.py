bfb4c68b65fe393748974f80c886387791ca9756
naoimportaweb <nao.importa.web@gmail.com>
2025-05-23 12:40:34 -0300

    Fiz uma class network para pegar meu IP

--- projects/api/network.py
+++ projects/api/network.py
@@ -9,6 +9,7 @@ from process import Process;
 class Network():
     def __init__(self):
         self.devices = [];
+        self.list_devices();
 
     def default(self):
         p = Process("ip route");
@@ -18,6 +19,20 @@ class Network():
                 return linha.split(" ")[4];
         return None;
 
+    def tunnels(self):
+        buffer = [];
+        for device in self.devices:
+            if device.type == "tun" and self.test( device.name ):
+                buffer.append( device.name );
+        return buffer;
+
+    def test(self, interface):
+        try:
+            self.location(interface=interface);
+            return True;
+        except:
+            return False;
+    
     def location(self, interface=None, proxy=None):
         if interface == None:
             interface = self.default();
@@ -83,7 +98,8 @@ class InterfaceAdapter(adapters.HTTPAdapter):
         )
 if __name__ == "__main__":
     n = Network();
+    print( n.tunnels() );
     #print( n.location() ); # ISSO AQUI VAI MOSTRAR O SEU IP NAO IMPORTA O QUE ESTEJA USANDO
-    print( n.location(interface="tun0") );
-    print( n.location(interface="tun0", proxy='socks5://127.0.0.1:9050') );
+    #print( n.location(interface="tun0") );
+    #print( n.location(interface="tun0", proxy='socks5://127.0.0.1:9050') );
 
